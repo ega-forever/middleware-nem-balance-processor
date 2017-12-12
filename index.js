e8f33b92b6f75903044eaad657225842b0ce3e22
Vyacheslav Frolov <algebris@gmail.com>
2017-12-13 02:16:06 +0300

    main logic
    MD-56

--- index.js
+++ index.js
@@ -1,4 +1,88 @@
-const init = async function () {
-	console.log('done');
+/**
+ * Middleware service for handling user balance. 
+ * Update balances for accounts, which addresses were specified
+ * in received transactions from blockParser via amqp
+ * 
+ * @module Chronobank/nem-balance-processor
+ * @requires config
+ * @requires models/accountModel
+ */
+
+const _ = require('lodash'),
+  Promise = require('bluebird'),
+  mongoose = require('mongoose'),
+  bunyan = require('bunyan'),
+  amqp = require('amqplib'),
+  config = require('./config'),
+  nis = require('./services/nisRequestService'),
+  accountModel = require('./models/accountModel'),
+  log = bunyan.createLogger({name: 'nem-blockprocessor'});
+
+const TX_QUEUE = `${config.rabbit.serviceName}_transaction`;
+
+mongoose.Promise = Promise;
+mongoose.connect(config.mongo.uri, {useMongoClient: true});
+
+mongoose.connection.on('disconnected', function () {
+  log.error('Mongo disconnected!');
+  process.exit(0);
+});
+
+const inspect = obj => console.log(require('util').inspect(obj, { showHidden: true, depth: null }));
+
+const flattenMosaics = mosObj =>
+  _.transform(mosObj, (acc, m) => acc[`${m.mosaicId.namespaceId}:${m.mosaicId.name}`] = m.quantity, {});
+
+const intersectByMosaic = (m1, m2) => 
+  _.intersection(_.keys(flattenMosaics(m1)), _.keys(flattenMosaics(m2)));
+
+const init = async () => {
+  let conn = await amqp.connect(config.rabbit.url)
+    .catch(() => {
+      log.error('Rabbitmq is not available!');
+      process.exit(0);
+    });
+
+  let channel = await conn.createChannel();
+
+  channel.on('close', () => {
+    log.error('Rabbitmq process has finished!');
+    process.exit(0);
+  });
+
+  try {
+    await channel.assertExchange('events', 'topic', {durable: false});
+    await channel.assertQueue(`${config.rabbit.serviceName}.balance_processor`);
+    await channel.bindQueue(`${config.rabbit.serviceName}.balance_processor`, 'events', `${TX_QUEUE}.*`);    
+  } catch(e) {
+    log.error(e);
+    channel = await conn.createChannel();
+  }
+  
+  channel.prefetch(2);
+
+  channel.consume(`${config.rabbit.serviceName}.balance_processor`, async (data) => {
+    try {
+      const block = JSON.parse(data.content.toString()),
+        addr = data.fields.routingKey.slice(TX_QUEUE.length + 1),
+        accObj = await nis.getAccount(addr),
+        balance = _.get(accObj, 'account.balance');
+
+      let accUpdateObj = balance ? {balance} : {};
+      
+      if(_.get(block, 'mosaics')) {
+        const accMosaics = (await nis.getMosaicsForAccount(addr)).data;
+        const commonKeys = intersectByMosaic(_.get(block, 'mosaics'), accMosaics);
+        accUpdateObj['mosaics'] = _.pick(flattenMosaics(accMosaics), commonKeys);
+        delete(accUpdateObj.mosaics['nem:xem']);
+      }
+      await accountModel.update({address: addr}, accUpdateObj);
+
+    } catch(e) {
+      log.error(e);
+    }
+    channel.ack(data);
+  });  
 };
+
 module.exports = init();
---
+++ services/nisRequestService.js
@@ -0,0 +1,35 @@
+const request = require('request-promise'),
+  config = require('../config'),
+  _ = require('lodash'),
+  { URL } = require('url'),
+  bunyan = require('bunyan'),
+  log = bunyan.createLogger({name: 'nemBalanceProcessor.requestService'});
+
+const baseUrl = config.nis.server;
+
+const getMosaicsForAccount = async addr => get(`/account/mosaic/owned?address=${addr}`);
+const getAccount = async addr => get(`/account/get?address=${addr}`);
+
+const get = query => makeRequest(query, 'GET');
+const post = (query, body) => makeRequest(query, 'POST', body);
+
+const makeRequest = (path, method, body) => {
+  const options = {
+    method,
+    body,
+    uri: new URL(path, baseUrl),
+    json: true
+  };
+  return request(options).catch(e => errorHandler(e));
+};
+
+const errorHandler = err => {
+  log.error(err.error.message);
+};
+
+module.exports = { 
+  blockHeight, 
+  getBlock,
+  getAccount,
+  getMosaicsForAccount
+};
